500072125f4d155af7494496002bf02d53e86b3e
Ferran Borreguero <ferranbt@protonmail.com>
2024-01-03 10:37:27 +0100

    Legacy Transaction RLP decoding

--- src/Transactions.sol
+++ src/Transactions.sol
@@ -2,8 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "./utils/RLPWriter.sol";
+import "Solidity-RLP/RLPReader.sol";
 
 library Transactions {
+    using RLPReader for RLPReader.RLPItem;
+    using RLPReader for RLPReader.Iterator;
+    using RLPReader for bytes;
+
     struct Legacy {
         address to;
         uint64 gas;
@@ -31,5 +36,24 @@ library Transactions {
         items[8] = RLPWriter.writeBytes(txStruct.s);
 
         return RLPWriter.writeList(items);
+    }
+
+    function decodeRLP(bytes memory rlp) internal pure returns (Legacy memory) {
+        Legacy memory txStruct;
+
+        RLPReader.RLPItem[] memory ls = rlp.toRlpItem().toList();
+        require(ls.length == 9, "invalid transaction");
+
+        txStruct.nonce = uint64(ls[0].toUint());
+        txStruct.gasPrice = uint64(ls[1].toUint());
+        txStruct.gas = uint64(ls[2].toUint());
+        txStruct.to = ls[3].toAddress();
+        txStruct.value = uint64(ls[4].toUint());
+        txStruct.data = ls[5].toBytes();
+        txStruct.v = ls[6].toBytes();
+        txStruct.r = ls[7].toBytes();
+        txStruct.s = ls[8].toBytes();
+
+        return txStruct;
     }
 }
--- test/Transactions.sol
+++ test/Transactions.sol
@@ -7,8 +7,8 @@ import "src/Transactions.sol";
 contract TestTransactions is Test {
     using Transactions for *;
 
-    function testLegacyTransactionRLP() public {
-        Transactions.Legacy memory t = Transactions.Legacy({
+    function testLegacyTransactionRLPEncoding() public {
+        Transactions.Legacy memory legacyTxn0 = Transactions.Legacy({
             to: address(0x095E7BAea6a6c7c4c2DfeB977eFac326aF552d87),
             gas: 50000,
             gasPrice: 10,
@@ -21,11 +21,17 @@ contract TestTransactions is Test {
             s: abi.encodePacked(hex"8a8fae537ce25ed8cb5af9adac3f141af69bd515bd2ba031522df09b97dd72b1")
         });
 
-        bytes memory rlp = Transactions.encodeRLP(t);
+        bytes memory rlp = Transactions.encodeRLP(legacyTxn0);
 
         bytes memory expected = abi.encodePacked(
             hex"f85f800a82c35094095e7baea6a6c7c4c2dfeb977efac326af552d870a801ba09bea4c4daac7c7c52e093e6a4c35dbbcf8856f1af7b059ba20253e70848d094fa08a8fae537ce25ed8cb5af9adac3f141af69bd515bd2ba031522df09b97dd72b1"
         );
         assertEq0(rlp, expected);
+
+        Transactions.Legacy memory legacyTxn1 = Transactions.decodeRLP(rlp);
+
+        // re-encode to validate that the decoding was correct
+        bytes memory rlp1 = Transactions.encodeRLP(legacyTxn1);
+        assertEq0(rlp1, expected);
     }
 }
