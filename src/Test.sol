e52af66e7def3a401c331d88fb9ee85af874d999
Ferran Borreguero <ferranbt@protonmail.com>
2024-01-06 11:15:50 +0100

    Add validation for --ffi enabled + suave binary in forge

--- src/Test.sol
+++ src/Test.sol
@@ -2,14 +2,61 @@
 pragma solidity ^0.8.8;
 
 import "./forge/Registry.sol";
+import "forge-std/Test.sol";
 
-contract SuaveEnabled {
+contract SuaveEnabled is Test {
     function setUp() public {
-        // TODO: Add checks to validate that:
-        // - User is running the test with ffi. Since vm.ffi is deployed as a contract, the error if ffi is not active
-        // is reported as a Suave.PeekerReverted error and it is not clear what the problem is.
-        // - Suave binary is on $PATH and Suave is running. This could be done with ffi calls to the suave binary.
-        // Put this logic inside `enable` itself.
+        string[] memory inputs = new string[](2);
+        inputs[0] = "suave1";
+        inputs[1] = "forge";
+
+        try vm.ffi(inputs) returns (bytes memory result) {
+            console.logBytes(result);
+        } catch (bytes memory reason) {
+            revert(detectFFIErrorMessage(reason));
+        }
+
         Registry.enable();
+    }
+
+    function detectFFIErrorMessage(bytes memory reason) internal pure returns (string memory) {
+        // Errors from cheatcodes are reported as 'CheatcodeError(string)' events
+        // 'eeaa9e6f' is the signature of the event
+        if (!isPrefix(hex"eeaa9e6f", reason)) {
+            return string(reason);
+        }
+
+        // retrieve the body of the event by removing the signature
+        bytes memory eventBody = new bytes(reason.length - 4);
+        for (uint256 i = 4; i < reason.length; i++) {
+            eventBody[i - 4] = reason[i];
+        }
+
+        // decode event as 'tuple(bytes message)' since it is equivalent to tuple(string)
+        (bytes memory message) = abi.decode(eventBody, (bytes));
+
+        // the prefix is 'FFI is disabled' in hex
+        if (isPrefix(hex"4646492069732064697361626c6564", message)) {
+            return "Suave <> Forge integration requires the --ffi flag to be enabled";
+        }
+
+        // the prefix is 'failed to execute command' in hex
+        if (isPrefix(hex"6661696c656420746f206578656375746520636f6d6d616e64", message)) {
+            return "Forge cannot locate the 'suave' binary. Is it installed in $PATH?";
+        }
+
+        return string(message);
+    }
+
+    function isPrefix(bytes memory prefix, bytes memory data) internal pure returns (bool) {
+        if (prefix.length > data.length) {
+            return false;
+        }
+        for (uint256 i = 0; i < prefix.length; i++) {
+            if (prefix[i] != data[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
