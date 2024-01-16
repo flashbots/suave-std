46bafe1fffdbab065d5d83001bb1bc5e05de23c9
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-01-16 13:43:39 +0000

    Add eth_sendBundle protocol

--- src/protocols/Bundle.sol
+++ src/protocols/Bundle.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "../suavelib/Suave.sol";
 import "solady/src/utils/LibString.sol";
-import "solady/src/utils/JSONParserLib.sol";
 
 // https://docs.flashbots.net/flashbots-auction/advanced/rpc-endpoint#eth_sendbundle
 library Bundle {
@@ -15,14 +14,32 @@ library Bundle {
         bytes[] txns;
     }
 
-    function sendBundle(string memory url, Bundle memory bundle) internal view {
+    function sendBundle(string memory url, BundleObj memory bundle) internal view {
         Suave.HttpRequest memory request = encodeBundle(bundle);
         request.url = url;
         Suave.doHTTPRequest(request);
     }
 
     function encodeBundle(BundleObj memory args) internal pure returns (Suave.HttpRequest memory) {
-        bytes memory params = abi.encodePacked("{", '"txs": [', txn, "],", '"blockNumber": "', args.blockNumber, '"}');
+        require(args.txns.length > 0, "Bundle: no txns");
+
+        bytes memory params = abi.encodePacked('{"blockNumber": "', LibString.toString(args.blockNumber), '", "txs": [');
+        for (uint256 i = 0; i < args.txns.length; i++) {
+            params = abi.encodePacked(params, '"', LibString.toHexString(args.txns[i]), '"');
+            if (i < args.txns.length - 1) {
+                params = abi.encodePacked(params, ",");
+            } else {
+                params = abi.encodePacked(params, "]");
+            }
+        }
+        if (args.minTimestamp > 0) {
+            params = abi.encodePacked(params, ', "minTimestamp": ', LibString.toString(args.minTimestamp));
+        }
+        if (args.maxTimestamp > 0) {
+            params = abi.encodePacked(params, ', "maxTimestamp": ', LibString.toString(args.maxTimestamp));
+        }
+        params = abi.encodePacked(params, "}");
+
         bytes memory body =
             abi.encodePacked('{"jsonrpc":"2.0","method":"eth_sendBundle","params":[', params, '],"id":1}');
 
