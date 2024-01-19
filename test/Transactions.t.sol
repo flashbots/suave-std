3e0d624c48da3dc16d5425db68f1a21c4e296637
Ferran Borreguero <ferranbt@protonmail.com>
2024-01-19 19:30:39 +0000

    Add eth_sendBundle protocol (#28)
    * add eth bundle protocol
    
    * Add eth_sendBundle protocol
    
    * sendBundle tweaks (#29)
    
    * remove unused url from bundle struct
    
    * fix failing test
    
    * fix failing test
    
    * revised tx types on bundle protocol (#33)
    
    * remove unused url from bundle struct
    
    * fix failing test
    
    * fix failing test
    
    * split tx struct into tx & request, rename to EIP155
    
    ---------
    
    Co-authored-by: brock smedley <2791467+zeroXbrock@users.noreply.github.com>

--- test/Transactions.t.sol
+++ test/Transactions.t.sol
@@ -7,8 +7,8 @@ import "src/Transactions.sol";
 contract TestTransactions is Test {
     using Transactions for *;
 
-    function testLegacyTransactionRLPEncoding() public {
-        Transactions.Legacy memory txnWithToAddress = Transactions.Legacy({
+    function testEIP155TransactionRLPEncoding() public {
+        Transactions.EIP155 memory txnWithToAddress = Transactions.EIP155({
             to: address(0x095E7BAea6a6c7c4c2DfeB977eFac326aF552d87),
             gas: 50000,
             gasPrice: 10,
@@ -24,9 +24,9 @@ contract TestTransactions is Test {
         bytes memory expected = abi.encodePacked(
             hex"f85f800a82c35094095e7baea6a6c7c4c2dfeb977efac326af552d870a801ba09bea4c4daac7c7c52e093e6a4c35dbbcf8856f1af7b059ba20253e70848d094fa08a8fae537ce25ed8cb5af9adac3f141af69bd515bd2ba031522df09b97dd72b1"
         );
-        _testLegacyTransaction(txnWithToAddress, expected);
+        _testEIP155Transaction(txnWithToAddress, expected);
 
-        Transactions.Legacy memory txnWithoutToAddress = Transactions.Legacy({
+        Transactions.EIP155 memory txnWithoutToAddress = Transactions.EIP155({
             to: address(0),
             gas: 50000,
             gasPrice: 10,
@@ -42,14 +42,14 @@ contract TestTransactions is Test {
         expected = abi.encodePacked(
             hex"f84b010a82c350800a021ba0754a33a9c37cfcf61cd61939fd93f5fe194b7d1ee6ef07490e8c880f3bd0d87da0715bd50fa2c24e2ce0ea595025a44a39ac238558882f9f07dd885ddc51839419"
         );
-        _testLegacyTransaction(txnWithoutToAddress, expected);
+        _testEIP155Transaction(txnWithoutToAddress, expected);
     }
 
-    function _testLegacyTransaction(Transactions.Legacy memory legacyTxn, bytes memory expectedRlp) public {
+    function _testEIP155Transaction(Transactions.EIP155 memory legacyTxn, bytes memory expectedRlp) public {
         bytes memory rlp = Transactions.encodeRLP(legacyTxn);
         assertEq0(rlp, expectedRlp);
 
-        Transactions.Legacy memory legacyTxn1 = Transactions.decodeRLP(rlp);
+        Transactions.EIP155 memory legacyTxn1 = Transactions.decodeRLP_EIP155(rlp);
 
         // re-encode to validate that the decoding was correct
         bytes memory rlp1 = Transactions.encodeRLP(legacyTxn1);
