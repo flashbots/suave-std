c474f35be4e8593259fae2cb2c2d6c75e0c0fa84
Ferran Borreguero <ferranbt@protonmail.com>
2024-03-27 08:34:14 +0000

    Add Gateway contract to make calls to external contracts (#72)
    * Add jsonrpc call() method
    
    * Introduce the Gateway contract

---
+++ test/Gateway.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "src/Gateway.sol";
+import "src/Test.sol";
+
+contract TestGateway is Test, SuaveEnabled {
+    function testGateway() public {
+        string memory endpoint = getEthJsonRPC();
+        Gateway gateway = new Gateway(endpoint, address(0x00000000219ab540356cBB839Cbe05303d7705Fa));
+        DepositContract depositContract = DepositContract(address(gateway));
+
+        bytes memory count = depositContract.get_deposit_count();
+        require(count.length > 0, "count is empty");
+    }
+
+    function getEthJsonRPC() public returns (string memory) {
+        try vm.envString("JSONRPC_ENDPOINT") returns (string memory endpoint) {
+            return endpoint;
+        } catch {
+            vm.skip(true);
+        }
+    }
+}
+
+interface DepositContract {
+    function get_deposit_count() external view returns (bytes memory);
+}
