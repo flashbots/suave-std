7af721f112c23ca1bd77281c99c8bfb88027bc4f
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2024-05-04 15:20:36 +0100

    update test

--- test/protocols/ChatGPT.t.sol
+++ test/protocols/ChatGPT.t.sol
@@ -7,24 +7,25 @@ import "src/protocols/ChatGPT.sol";
 
 contract ChatGPTTest is Test, SuaveEnabled {
     function testChatGPT() public {
-        ChatGPT chatgpt = getChatGPT();
+        (ChatGPT chatgpt, string memory apiKey) = getChatGPT();
 
         ChatGPT.Message[] memory messages = new ChatGPT.Message[](1);
         messages[0] = ChatGPT.Message(ChatGPT.Role.User, "Say this is a test!");
 
         string memory expected = "This is a test!";
-        string memory found = chatgpt.complete(messages);
+        string memory found = chatgpt.complete(apiKey, messages);
 
         assertEq(found, expected, "ChatGPT did not return the expected result");
     }
 
-    function getChatGPT() public returns (ChatGPT chatgpt) {
+    function getChatGPT() public returns (ChatGPT chatgpt, string memory apiKey) {
         // NOTE: tried to do it with envOr but it did not worked
-        try vm.envString("CHATGPT_API_KEY") returns (string memory apiKey) {
-            if (bytes(apiKey).length == 0) {
+        try vm.envString("CHATGPT_API_KEY") returns (string memory apiKeyEnv) {
+            if (bytes(apiKeyEnv).length == 0) {
                 vm.skip(true);
             }
-            chatgpt = new ChatGPT(apiKey);
+            chatgpt = new ChatGPT();
+            apiKey = apiKeyEnv;
         } catch {
             vm.skip(true);
         }
