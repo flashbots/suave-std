38c249d534c6f71a09a9d71ba9629163b055d74d
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-05-22 16:43:02 -0700

    overload SentBundle w/ more param

--- src/Observable.sol
+++ src/Observable.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.23;
 
 abstract contract ObservableOrderflow {
     event SentBundle(bytes32 bundleHash);
+    event SentBundle(bytes32 bundleHash, bytes32[] txHashes);
     event SentTransaction(bytes32 txHash);
     event SentTransactions(bytes32[] txHashes);
 }
