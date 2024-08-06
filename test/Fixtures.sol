11d102814ac5c6aba491f561254505346c5f5f7b
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-08-06 09:32:11 +0100

    Do not use same interface names as Forge to avoid collusions

--- test/Fixtures.sol
+++ test/Fixtures.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-interface vmSafe {
+interface fixturesVmSafeRef {
     function readFile(string calldata path) external view returns (string memory data);
 }
 
 library Fixtures {
-    vmSafe constant vm = vmSafe(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    fixturesVmSafeRef constant vm = fixturesVmSafeRef(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     function readFixture(string memory path) internal view returns (string memory) {
         string memory fullPath = string.concat("./test/fixtures/", path);
